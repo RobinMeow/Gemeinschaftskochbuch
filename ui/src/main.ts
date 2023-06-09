21b0d5679116b9885347e6e792bca85d0ad817e7
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-09 16:17:45 +0200

    add angular material

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,9 +2,12 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app/app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
     providers: [
-      provideRouter(APP_ROUTES),
-    ]
+    provideRouter(APP_ROUTES),
+    importProvidersFrom(BrowserAnimationsModule)
+]
 }).catch(err => console.error(err));
