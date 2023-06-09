1e4a42881e02e1b3095ab08238230c6547f2ddf1
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-09 10:45:36 +0200

    fix compiler warnings CS8618

--- api/Controllers/RezeptDto.cs
+++ api/Controllers/RezeptDto.cs
@@ -5,6 +5,6 @@ namespace api.Controllers;
 public sealed class RezeptDto
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime Erstelldatum { get; set; }
 }
--- api/Domain/Rezept.cs
+++ api/Domain/Rezept.cs
@@ -5,6 +5,6 @@ namespace api.Domain;
 public sealed class Rezept
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime Erstelldatum { get; set; }
 }
