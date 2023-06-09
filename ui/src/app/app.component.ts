2be0e9de588eebcc7a2c20deae15c638c69f70b6
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-09 14:54:41 +0200

    set add-rezept comp as start up view temporarly

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { AddRezeptComponent } from "./src/app/add-rezept/add-rezept.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [ 
-      NgSwitch,
-      NgSwitchDefault,
-      NgSwitchCase,
-      RouterOutlet
+    imports: [
+        NgSwitch,
+        NgSwitchDefault,
+        NgSwitchCase,
+        RouterOutlet,
+        AddRezeptComponent
     ]
 })
 export class AppComponent {
