669137bbe75707b65f534add8aedcde32425d197
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-09 16:13:25 +0200

    add validations to name

--- ui/src/app/src/app/add-rezept/add-rezept.component.ts
+++ ui/src/app/src/app/add-rezept/add-rezept.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-rezept',
@@ -13,11 +13,27 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./add-rezept.component.scss']
 })
 export class AddRezeptComponent {
-  protected rezeptForm: FormGroup = new FormGroup({
-    name: new FormControl()
-  });
+  protected rezeptForm: FormGroup;
+
+  constructor(fromBuilder: FormBuilder) {
+    this.rezeptForm = fromBuilder.group({
+      name: ['', [ // default value, than array of validators (you can pass in a second array for async validators. I should check it out)
+        Validators.required,
+        Validators.minLength(3), 
+        Validators.maxLength(50)
+      ]] 
+    });
+
+    // use for auto completion and stuff
+    // this.rezeptForm.valueChanges.subscribe(); 
+
+    // This shows how to make a dynamic form for the Zutaten, which can be any number, so the html should be generated based on it..
+    // https://youtu.be/JeeUY6WaXiA?t=355
+  }
 
   protected onAdd(): void {
     console.log(this.rezeptForm.value);
   }
 }
+
+// Idea: On the Zutaten. Whener ever a Zutat-name is supplied, (the first character to be excat) it should automatically raise "add Zutat" button. On Submit it needs to filter out the last one, if it is empty ..
