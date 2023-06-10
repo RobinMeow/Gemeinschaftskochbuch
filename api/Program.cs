25cfe831997825e509b53d4024814a9ea75bc78a
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-10 19:45:13 +0200

    fix CORS problem

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,7 +17,16 @@ internal class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
+
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCors((CorsOptions corsOptions) => {
+            corsOptions.AddDefaultPolicy((CorsPolicyBuilder corsPolicyBuilder) => {
+                corsPolicyBuilder.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
 
         WebApplication app = builder.Build();
 
@@ -25,11 +36,11 @@ internal class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        else // prevent warn: Microsoft.AspNetCore.HttpsPolicy.HttpsRedirectionMiddleware[3] Failed to determine the https port for redirect.
+            app.UseHttpsRedirection();
 
-        app.UseHttpsRedirection();
-
+        app.UseCors();
         app.UseAuthorization();
-
         app.MapControllers();
 
         app.Run();
