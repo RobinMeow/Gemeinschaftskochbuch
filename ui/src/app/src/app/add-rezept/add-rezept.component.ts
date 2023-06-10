1119e3ed7928da9422e495edcd910f0984d8f307
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-10 11:13:52 +0200

    add http addRezept call with validation

--- ui/src/app/src/app/add-rezept/add-rezept.component.ts
+++ ui/src/app/src/app/add-rezept/add-rezept.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { Observable, catchError } from 'rxjs';
+import { HandleError, HttpErrorHandler } from 'src/app/http-error-handler.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    // Authorization: 'my-auth-token'
+  })
+};
 
 @Component({
   selector: 'app-add-rezept',
@@ -17,15 +27,20 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./add-rezept.component.scss']
 })
 export class AddRezeptComponent {
-  // not marked as static, because I want to access them from the template 
+  // not marked as static, because I want to access them from the template
   protected readonly NAME_MIN_LENGTH: number = 3;
   protected readonly NAME_MAX_LENGTH: number = 50;
-  
+  private _handleError: HandleError; // https://angular.io/guide/http#sending-data-to-a-server
+
   protected rezeptForm: FormGroup;
 
   get name(): AbstractControl<any, any> { return this.rezeptForm.get('name')!; }
 
-  constructor(fromBuilder: FormBuilder) {
+  constructor(
+    fromBuilder: FormBuilder,
+    private _httpClient: HttpClient,
+    httpErrorHandler: HttpErrorHandler
+    ) {
     this.rezeptForm = fromBuilder.group({
       name: ['', [ // default value, than array of validators (you can pass in a second array for async validators. I should check it out)
         Validators.required,
@@ -40,11 +55,42 @@ export class AddRezeptComponent {
 
     // This shows how to make a dynamic form for the Zutaten, which can be any number, so the html should be generated based on it..
     // https://youtu.be/JeeUY6WaXiA?t=355
+
+    this._handleError = httpErrorHandler.createHandleError('HeroesService');
   }
 
   protected onAdd(): void {
     console.log(this.rezeptForm.value);
+
+    if (!this.nameIsValid()) return;
+
+    const rezept: Rezept = {
+      name: this.name.value
+    };
+
+    this.addRezept(rezept);
   }
+
+  private nameIsValid(): boolean {
+    const nameValue: string = this.name.value;
+
+    if (nameValue.length < this.NAME_MIN_LENGTH) return false;
+    if (nameValue.length > this.NAME_MAX_LENGTH) return false;
+    if (nameValue.length == 0) return false;
+
+    return true;
+  }
+
+  addRezept(rezept: Rezept): Observable<Rezept> {
+    return this._httpClient.post<Rezept>('http://localhost:5263' + '/Rezepte/Add', rezept, httpOptions)
+    .pipe(
+      catchError(this._handleError('addRezept', rezept))
+    );
+  }
+}
+
+interface Rezept {
+  name: string;
 }
 
 // Idea: On the Zutaten. Whener ever a Zutat-name is supplied, (the first character to be excat) it should automatically raise "add Zutat" button. On Submit it needs to filter out the last one, if it is empty ..
