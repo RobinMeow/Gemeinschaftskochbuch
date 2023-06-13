df67865599636649ebbacd1f01aaf4f0c53b390c
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-06-13 23:55:50 +0200

    further improve DateTimeSerializer performance and readability

--- api/Infrastructure/DateTimeSerializer.cs
+++ api/Infrastructure/DateTimeSerializer.cs
@@ -4,14 +4,15 @@ using MongoDB.Bson.Serialization;
 
 namespace api.Infrastructure;
 
-public class DateTimeSerializer : IBsonSerializer<DateTime>
+public sealed class DateTimeSerializer : IBsonSerializer<DateTime>
 {
     public Type ValueType => typeof(DateTime);
 
     static readonly CultureInfo InvariantCulture = CultureInfo.InvariantCulture; // cache for performance
 
-    static readonly string[] allowedIsoFormats = new[] {
-        "yyyy-MM-dd'T'HH:mm:ss.fffffff'Z'",
+    ///<summary>Array of allowed ISO 8601 date-time formats ordered in likelyness to occur.</summary>
+    static readonly string[] AllowedIsoFormats = new[] {
+        "yyyy-MM-dd'T'HH:mm:ss.fffffff'Z'", // DateTime.UtcNow.ToISOString() produces 7 fractional seconds usually. It is unlikely, to call it at excat 0000000 fractional seconds (production only)
         "yyyy-MM-dd'T'HH:mm:ss.ffffff'Z'",
         "yyyy-MM-dd'T'HH:mm:ss.fffff'Z'",
         "yyyy-MM-dd'T'HH:mm:ss.ffff'Z'",
@@ -21,6 +22,7 @@ public class DateTimeSerializer : IBsonSerializer<DateTime>
         "yyyy-MM-dd'T'HH:mm:ss'Z'"
     };
 
+    /// <summary>Deserializes a DateTime value from BSON.</summary>
     public DateTime Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
         // MongoDB C# Driver Version (Src: https://github.com/mongodb/mongo-csharp-driver/blob/master/src/MongoDB.Bson/Serialization/Serializers/DateTimeSerializer.cs)
@@ -28,12 +30,13 @@ public class DateTimeSerializer : IBsonSerializer<DateTime>
 
         string dateTimeIsoString = context.Reader.ReadString();
 
-        if (DateTime.TryParseExact(dateTimeIsoString, allowedIsoFormats, InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime parsedDate)) // TryParseExact is faster than Parse, if a single format is specified
+        if (DateTime.TryParseExact(dateTimeIsoString, AllowedIsoFormats, InvariantCulture, DateTimeStyles.AssumeUniversal, out DateTime parsedDate)) // TryParseExact should be faster than Parse, or TryParse
             return parsedDate;
         else
             throw new ArgumentException($"Expected value of type {nameof(String)} in ISO format, but got '{dateTimeIsoString}'.");
     }
 
+    /// <summary>Serializes a DateTime value to BSON.</summary>
     public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, DateTime value)
     {
         // MongoDB C# Driver Version (Src: https://github.com/mongodb/mongo-csharp-driver/blob/master/src/MongoDB.Bson/Serialization/Serializers/DateTimeSerializer.cs)
@@ -43,16 +46,18 @@ public class DateTimeSerializer : IBsonSerializer<DateTime>
         context.Writer.WriteString(dateTimeIso);
     }
 
+    /// <summary>Deserializes a DateTime value from BSON.</summary>
+    object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        return Deserialize(context, args);
+    }
+
+    /// <summary>Serializes a DateTime value to BSON.</summary>
     public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
     {
         if (value is DateTime dateTime)
             Serialize(context, args, dateTime);
         else
             throw new ArgumentException($"Expected value of type {typeof(DateTime)}, but got {value?.GetType()}.");
-    }
-
-    object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
-    {
-        return Deserialize(context, args);
     }
 }
