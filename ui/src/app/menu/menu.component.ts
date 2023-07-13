385a62043fc58c903bbec983d41500ded4012b36
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-07-13 14:45:21 +0200

    add menu toggeling

--- ui/src/app/menu/menu.component.ts
+++ ui/src/app/menu/menu.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
