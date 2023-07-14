9139a9e530c546435717a8c0e03154e3e8c878ea
RobinMeow <111490292+RobinMeow@users.noreply.github.com>
2023-07-14 17:21:58 +0200

    move AccessControlAllowOrigin into interceptor

--- ui/src/app/token.interceptor.ts
+++ ui/src/app/token.interceptor.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
 import { Observable, from, mergeMap } from 'rxjs';
 import { AuthService } from './auth.service';
+import { FRONTEND_ORIGINS } from './app.tokens';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private _auth: AuthService) {}
+  constructor(
+    private _auth: AuthService,
+    @Inject(FRONTEND_ORIGINS) private _frontendOrigins: string[],
+    ) {
+    }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // ToDo: If api
@@ -23,7 +28,8 @@ export class TokenInterceptor implements HttpInterceptor {
         if (token != undefined) {
           request = request.clone({
             setHeaders: {
-              Authorization: `Bearer ${token}`
+              Authorization: `Bearer ${token}`,
+              'Access-Control-Allow-Origin': this._frontendOrigins.join(',')
             }
           });
         }
